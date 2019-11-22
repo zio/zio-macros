756bf300ba94ec4a88ee92de5cf3bc502cf5c749
Maxim Schuwalow <maxim.schuwalow@zalando.de>
2019-11-22 14:52:56 +0100

    update naming

--- core/shared/src/main/scala/zio/macros/delegate/ZScheduleSyntax.scala
+++ core/shared/src/main/scala/zio/macros/delegate/ZScheduleSyntax.scala
@@ -9,20 +9,20 @@ trait ZScheduleSyntax {
 
   implicit class ZScheduleSyntax[R, A, B](zSchedule: ZSchedule[R, A, B]) {
 
-    def delayed0[R1 <: R with Clock](f: Duration => Duration)(implicit ev: R Mix Clock): ZSchedule[R1, A, B] =
+    def delayed_[R1 <: R with Clock](f: Duration => Duration)(implicit ev: R Mix Clock): ZSchedule[R1, A, B] =
       zSchedule.delayedEnv(f)(g => patch[R, Clock].apply(c => new Clock { val clock = g(c.clock) }))
 
-    def delayedM0[R1 <: R with Clock](
+    def delayedM_[R1 <: R with Clock](
       f: Duration => ZIO[R1, Nothing, Duration]
     )(implicit ev: R Mix Clock): ZSchedule[R1, A, B] =
       zSchedule.delayedMEnv(f)(g => patch[R, Clock].apply(c => new Clock { val clock = g(c.clock) }))
 
-    def jittered0[R1 <: R with Clock with Random](min: Double = 0.0, max: Double = 1.0)(
+    def jittered_[R1 <: R with Clock with Random](min: Double = 0.0, max: Double = 1.0)(
       implicit ev: R Mix Clock
     ): ZSchedule[R1, A, B] =
       zSchedule.jitteredEnv(min, max)(f => patch[R, Clock].apply(c => new Clock { val clock = f(c.clock) }))
 
-    def modifyDelay0[R1 <: R with Clock](
+    def modifyDelay_[R1 <: R with Clock](
       f: (B, Duration) => ZIO[R1, Nothing, Duration]
     )(implicit ev: R Mix Clock): ZSchedule[R1, A, B] =
       zSchedule.modifyDelayEnv(f)(g => patch[R, Clock].apply(c => new Clock { val clock = g(c.clock) }))
